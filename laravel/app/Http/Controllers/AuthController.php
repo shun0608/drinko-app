d4e02871d46c3ea7865fcc09127256c184ed686e
shun0608 <bmss.4124@icloud.com>
2024-06-09 14:31:59 +0900

    ・ミドルウェア設定 ・ログイン用のコントローラー作成

---
+++ laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class AuthController extends Controller
+{
+  public function login(Request $request)
+  {
+    $credentials = $request->validate([
+      'email' => ['required', 'email'],
+      'password' => ['required'],
+    ]);
+
+
+    if (Auth::attempt($credentials)) {
+      $request->session()->regenerate();
+      return response()->json(['message' => 'ログイン成功']);
+    }
+    // 以下の1行は要検討
+    return response()->json(['message' => '認証に失敗しました'], 401);
+  }
+}
--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -5,14 +5,21 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+  ->withRouting(
+    web: __DIR__ . '/../routes/web.php',
+    commands: __DIR__ . '/../routes/console.php',
+    health: '/up',
+  )
+  ->withMiddleware(function (Middleware $middleware) {
+    $middleware->group('api', [
+      // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+      // 'throttle:api',
+      \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
+      \Illuminate\Routing\Middleware\SubstituteBindings::class,
+      \App\Http\Middleware\EncryptCookies::class,
+      \Illuminate\Session\Middleware\StartSession::class,
+    ]);
+  })
+  ->withExceptions(function (Exceptions $exceptions) {
+    //
+  })->create();
