159a27253c6f6008f65fe0058ee53856a842d668
shun0608 <bmss.4124@icloud.com>
2024-06-09 15:08:35 +0900

    ルーティング作成

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Models\User;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+  Route::get('/user', function (Request $request) {
+    $user = $request->user();
+    \Log::info($user);
+    return $user;
+  });
+  Route::post('/posts', function (Request $request) {
+    $users = User::all();
+    return response()->json(compact('users'), 200);
+  });
+});
