53397cf8139f8aac7c94e07a64c21bfdf074912b
shun0608 <bmss.4124@icloud.com>
2024-06-17 21:26:15 +0900

    コマンドを段階ごとに分割

--- laravel/app/Console/Commands/FetchDrinkDetailsFromApi.php
+++ laravel/app/Console/Commands/FetchDrinkDetailsFromApi.php
@@ -4,54 +4,27 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-use Illuminate\Support\Facades\Http;
-
-use App\Models\Drink;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7;
-use GuzzleHttp\Exception\ClientException;
-
-class GetDrinksCommand extends Command
+class FetchDrinkDetailsFromApi extends Command
 {
   /**
    * The name and signature of the console command.
    *
    * @var string
    */
-  protected $signature = 'app:get-drinks-command';
+  protected $signature = 'app:fetch-drink-details-from-api';
 
   /**
    * The console command description.
    *
    * @var string
    */
-  protected $description = 'Get drink data from TheCocktailDB';
+  protected $description = 'Fetch detailed drink recipes based on drinks_id from TheCocktailDB API';
 
   /**
    * Execute the console command.
    */
   public function handle()
   {
-    // $non_alcohol_response = Http::get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic');
-
-    // try {
-    //   $data = $non_alcohol_response;
-    // } catch (ClientException $e) {
-    //   echo Psr7\Message::toString($e->getRequest());
-    //   echo Psr7\Message::toString($e->getResponse());
-    // }
-    // if ($data) {
-    //   $data = json_decode($data, true);
-    // }
-
-    // foreach ($data['drinks'] as $drink_element) {
-    //   $drink = new Drink();
-    //   $drink->drink_id = $drink_element['idDrink'];
-    //   $drink->name_en = $drink_element['strDrink'];
-    //   $drink->image_url = $drink_element['strDrinkThumb'];
-    //   $drink->save();
-    // }
-
     $drink_ids = Drink::select('drink_id')->get();
     $drink_detail_url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
