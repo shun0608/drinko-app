d06502d1298ca38f87cddb37a9965a85b80cb8a9
shun0608 <bmss.4124@icloud.com>
2024-06-24 17:05:58 +0900

    UserRequestのユニーク制約修正

--- laravel/app/Http/Requests/User/UserRequest.php
+++ laravel/app/Http/Requests/User/UserRequest.php
@@ -26,19 +26,10 @@ class UserRequest extends FormRequest
   {
     return [
       //実装時は、
-      // 'email' => ['required', 'string', 'email:strict,dns,spoof', 'max:255', 'unique:users'],
+      // 'email' => ['required', 'string', 'email:strict,dns,spoof', 'max:255', 'unique:users,email,NULL,id,deleted_at,NULL'],
       'name' => ['required', 'max:255'],
-      'email' => ['required', 'string', 'email:rfc', 'max:255', 'unique:users'],
+      'email' => ['required', 'string', 'email:rfc', 'max:255', 'unique:users,email,NULL,id,deleted_at,NULL'],
       'password' => ['required', 'string', Password::min(8)]
     ];
   }
-
-  // protected function failedValidation(Validator $validator)
-  // {
-  //   $res = response()->json([
-  //     'status' => 400,
-  //     'errors' => $validator->errors(),
-  //   ], 400);
-  //   throw new HttpResponseException($res);
-  // }
 }
--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -6,38 +6,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Auth\Passwords\CanResetPassword;
 
 class User extends Authenticatable
 {
-  use HasFactory, Notifiable;
+  use HasFactory, Notifiable, SoftDeletes;
 
-  /**
-   * The attributes that are mass assignable.
-   *
-   * @var array<int, string>
-   */
   protected $fillable = [
     'name',
     'email',
     'password',
   ];
 
-  /**
-   * The attributes that should be hidden for serialization.
-   *
-   * @var array<int, string>
-   */
+  protected $dates = ['deleted_at'];
+
   protected $hidden = [
     'password',
     'remember_token',
   ];
 
-  /**
-   * Get the attributes that should be cast.
-   *
-   * @return array<string, string>
-   */
   protected function casts(): array
   {
     return [
