48e958b4c5f14e4500b05fa22b609397df8a9707
shun0608 <bmss.4124@icloud.com>
2024-07-13 17:23:00 +0900

    ・Variable $fetchedDrinkDetails might not be defined.エラー修正 ・エラーハンドリングをHTTPリクエストに準拠する形で修正 ・コマンド成功時1を失敗時0を返すよう修正

--- laravel/app/Console/Commands/FetchDrinkDetailsFromApi.php
+++ laravel/app/Console/Commands/FetchDrinkDetailsFromApi.php
@@ -11,33 +11,35 @@ use App\Models\Drink;
 
 class FetchDrinkDetailsFromApi extends Command
 {
-  protected $signature = 'app:fetch-drink-details-from-api';
+    protected $signature = 'app:fetch-drink-details-from-api';
 
-  protected $description = 'Fetch detailed drink recipes based on drinks_id from TheCocktailDB API';
+    protected $description = 'Fetch detailed drink recipes based on drinks_id from TheCocktailDB API';
 
-  public function handle()
-  {
-    $drinks = Drink::select('cocktailDB_id')->get();
-    $drink_details_url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+    public function handle()
+    {
+        $drinks = Drink::select('cocktailDB_id')->get();
+        $drink_details_url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
-    foreach ($drinks as $drink) {
-      try {
-        $fetchedDrinkDetails = Http::get($drink_details_url . $drink['cocktailDB_id']);
-      } catch (ClientException $e) {
-        echo Psr7\Message::toString($e->getRequest());
-        echo Psr7\Message::toString($e->getResponse());
-      }
-      $fetchedDrinkDetails = json_decode($fetchedDrinkDetails, true);
-      $drink_details = $fetchedDrinkDetails['drinks'][0];
-      $drink = Drink::where('cocktailDB_id', $drink['cocktailDB_id'])->first();
-      $drink->recipe_en = $drink_details['strInstructions'];
-      for ($i = 1; $i <= 15; $i++) {
-        $ingredient_property = 'ingredient_en' . $i;
-        $measure_property = 'measure_en' . $i;
-        $drink->$ingredient_property = $drink_details['strIngredient' . $i];
-        $drink->$measure_property = $drink_details['strMeasure' . $i];
-      }
-      $drink->save();
+        foreach ($drinks as $drink) {
+            $fetchedDrinkDetails = Http::get($drink_details_url . $drink['cocktailDB_id']);
+
+            if ($fetchedDrinkDetails->failed()) {
+                $this->error('Request failed: ' . $fetchedDrinkDetails->body());
+                return 0;
+            }
+
+            $fetchedDrinkDetails = json_decode($fetchedDrinkDetails, true);
+            $drink_details = $fetchedDrinkDetails['drinks'][0];
+            $drink = Drink::where('cocktailDB_id', $drink['cocktailDB_id'])->first();
+            $drink->recipe_en = $drink_details['strInstructions'];
+            for ($i = 1; $i <= 15; $i++) {
+                $ingredient_property = 'ingredient_en' . $i;
+                $measure_property = 'measure_en' . $i;
+                $drink->$ingredient_property = $drink_details['strIngredient' . $i];
+                $drink->$measure_property = $drink_details['strMeasure' . $i];
+            }
+            $drink->save();
+        }
+        return 1;
     }
-  }
 }
