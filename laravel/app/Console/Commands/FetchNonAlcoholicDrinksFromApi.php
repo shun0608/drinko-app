96a4b4784768ff2e11bcb9ba3701388236b19a71
shun0608 <bmss.4124@icloud.com>
2024-07-13 17:18:23 +0900

    ・Dead catchエラーがでていたため、try,catchブロック削除 ・HTTPレスポンスがfailedの際の記述追加 ・失敗時0,成功時1を返すよう修正

--- laravel/app/Console/Commands/FetchNonAlcoholicDrinksFromApi.php
+++ laravel/app/Console/Commands/FetchNonAlcoholicDrinksFromApi.php
@@ -9,32 +9,32 @@ use Illuminate\Support\Facades\Http;
 use App\Models\Drink;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7;
-use GuzzleHttp\Exception\ClientException;
 
 class FetchNonAlcoholicDrinksFromApi extends Command
 {
-  protected $signature = 'app:fetch-non-alcoholic-drinks-from-api';
+    protected $signature = 'app:fetch-non-alcoholic-drinks-from-api';
 
-  protected $description = 'Fetch non alcoholic drink data from TheCocktailDB';
+    protected $description = 'Fetch non alcoholic drink data from TheCocktailDB';
 
-  public function handle()
-  {
-    $non_alcoholic_response = Http::get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic');
+    public function handle()
+    {
+        $non_alcoholic_response = Http::get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic');
 
-    try {
-      $data = $non_alcoholic_response;
-    } catch (ClientException $e) {
-      echo Psr7\Message::toString($e->getRequest());
-      echo Psr7\Message::toString($e->getResponse());
+        if ($non_alcoholic_response->failed()) {
+            $this->error('Request failed: ' . $non_alcoholic_response->body());
+            return 0;
+        }
+
+        $data = $non_alcoholic_response;
+        $data = json_decode($data, true);
+
+        foreach ($data['drinks'] as $drink_element) {
+            $drink = new Drink();
+            $drink->cocktailDB_id = $drink_element['idDrink'];
+            $drink->name_en = $drink_element['strDrink'];
+            $drink->image_url = $drink_element['strDrinkThumb'];
+            $drink->save();
+        }
+        return 1;
     }
-    $data = json_decode($data, true);
-
-    foreach ($data['drinks'] as $drink_element) {
-      $drink = new Drink();
-      $drink->cocktailDB_id = $drink_element['idDrink'];
-      $drink->name_en = $drink_element['strDrink'];
-      $drink->image_url = $drink_element['strDrinkThumb'];
-      $drink->save();
-    }
-  }
 }
