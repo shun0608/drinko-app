323841cd4c0d251c48117ec84b56ab19767df1ac
shun0608 <bmss.4124@icloud.com>
2024-07-13 16:28:49 +0900

    AuthRequestのValidatorインターフェースをPSR-4標準に準拠

--- laravel/app/Http/Requests/User/AuthRequest.php
+++ laravel/app/Http/Requests/User/AuthRequest.php
@@ -2,30 +2,30 @@
 
 namespace App\Http\Requests\User;
 
-use Illuminate\contracts\Validation\Validator;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
 class AuthRequest extends FormRequest
 {
-  /**
-   * Determine if the user is authorized to make this request.
-   */
-  public function authorize(): bool
-  {
-    return true;
-  }
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
 
-  /**
-   * Get the validation rules that apply to the request.
-   *
-   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-   */
-  public function rules(): array
-  {
-    return [
-      'email' => ['required', 'string', 'email:strict,dns,spoof', 'max:255'],
-      'password' => ['required', 'string']
-    ];
-  }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email:strict,dns,spoof', 'max:255'],
+            'password' => ['required', 'string']
+        ];
+    }
 }
