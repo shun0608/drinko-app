a35f0eea59c4b7b47c674dfa2eadf2a7318a41ba
shun0608 <bmss.4124@icloud.com>
2024-07-13 16:27:41 +0900

    UserRequestのValidatorインターフェースをSR-4標準に準拠

--- laravel/app/Http/Requests/User/UserRequest.php
+++ laravel/app/Http/Requests/User/UserRequest.php
@@ -2,32 +2,32 @@
 
 namespace App\Http\Requests\User;
 
-use Illuminate\contracts\Validation\Validator;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules\Password;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserRequest extends FormRequest
 {
-  /**
-   * Determine if the user is authorized to make this request.
-   */
-  public function authorize(): bool
-  {
-    return true;
-  }
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
 
-  /**
-   * Get the validation rules that apply to the request.
-   *
-   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-   */
-  public function rules(): array
-  {
-    return [
-      'name' => ['required', 'max:255'],
-      'email' => ['required', 'string', 'email:strict,dns,spoof', 'max:255', 'unique:users,email,NULL,id,deleted_at,NULL'],
-      'password' => ['required', 'string', Password::min(8)]
-    ];
-  }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'max:255'],
+            'email' => ['required', 'string', 'email:strict,dns,spoof', 'max:255', 'unique:users,email,NULL,id,deleted_at,NULL'],
+            'password' => ['required', 'string', Password::min(8)]
+        ];
+    }
 }
