a7f0a1e6c3cdf2c06df1eb0f3278496f3477603e
shun0608 <bmss.4124@icloud.com>
2024-07-14 14:32:28 +0900

    drinks/[id].vue修正

--- nuxt/pages/drinks/[id].vue
+++ nuxt/pages/drinks/[id].vue
@@ -19,12 +19,15 @@ const { drink, recommended: drinks } = await $apiFetch(
 
 const drinkIngredients = [];
 for (let i = 1; i <= 15; i++) {
-  let ingredientKey = "ingredient_ja" + i;
-  let measureKey = "measure_ja" + i;
-  let ingredient = drink[ingredientKey];
-  let measure = drink[measureKey];
+  const ingredientKey = "ingredient_ja" + i;
+  const measureKey = "measure_ja" + i;
+  const ingredient = drink[ingredientKey];
+  const measure = drink[measureKey];
   if (ingredient && measure) {
-    drinkIngredients.push(`${ingredient} ${measure}`);
+    drinkIngredients.push({
+      id: i,
+      text: `${ingredient} ${measure}`,
+    });
   }
 }
 
@@ -73,80 +76,82 @@ onMounted(updateIsFavoriteStatus);
 </script>
 
 <template>
-  <UseSeoMeta
-    :title="`${drink.name_en}`"
-    :description="`${drink.recipe_ja}`"
-    :image-url="`${drink.image_url}`"
-  />
-  <div class="max-w-screen-lg mx-auto mt-8 mb-28">
-    <UBreadcrumb
-      :links="links"
-      :ui="{
-        wrapper: 'mx-4',
-        li: 'text-primary dark:text-primary',
-        active: 'text-primary dark:text-primary-400',
-        inactive: 'hover:text-gray-700 dark:hover:text-gray-200',
-      }"
+  <div>
+    <UseSeoMeta
+      :title="`${drink.name_en}`"
+      :description="`${drink.recipe_ja}`"
+      :image-url="`${drink.image_url}`"
     />
-    <div class="mx-4">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-16 mt-5">
-        <figure class="">
-          <img
-            :src="drink.image_url"
-            :alt="drink.name_en"
-            class="aspect-square object-cover w-full"
-          />
-        </figure>
-        <div>
-          <h1 class="text-3xl font-bold">{{ drink.name_en }}</h1>
-          <h4 class="text-md font-bold mt-3">{{ drink.name_ja }}</h4>
-          <h5 class="text-lg mt-5">材料</h5>
-          <ul class="list-disc list-inside ms-1">
-            <li v-for="ingredient in drinkIngredients">
-              {{ ingredient }}
-            </li>
-          </ul>
-          <h5 class="text-lg mt-5">作り方</h5>
-          <p class="mt-1">{{ drink.recipe_ja }}</p>
-          <div class="flex flex-row flex-wrap gap-2 mt-5">
-            <button
-              :class="[
-                'btn btn-sm h-10 text-xs rounded-md',
-                { favorite: isFavorite },
-              ]"
-              @click="updateFavorite()"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                :fill="isFavorite ? 'white' : 'none'"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-              お気に入り
-            </button>
-            <SocialShare
-              v-for="network in ['facebook', 'twitter', 'line']"
-              :key="network"
-              :network="network"
-              :styled="true"
-              :label="true"
-              class="text-white h-10"
+    <div class="max-w-screen-lg mx-auto mt-8 mb-28">
+      <UBreadcrumb
+        :links="links"
+        :ui="{
+          wrapper: 'mx-4',
+          li: 'text-primary dark:text-primary',
+          active: 'text-primary dark:text-primary-400',
+          inactive: 'hover:text-gray-700 dark:hover:text-gray-200',
+        }"
+      />
+      <div class="mx-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-16 mt-5">
+          <figure class="">
+            <img
+              :src="drink.image_url"
+              :alt="drink.name_en"
+              class="aspect-square object-cover w-full"
             />
+          </figure>
+          <div>
+            <h1 class="text-3xl font-bold">{{ drink.name_en }}</h1>
+            <h4 class="text-md font-bold mt-3">{{ drink.name_ja }}</h4>
+            <h5 class="text-lg mt-5">材料</h5>
+            <ul class="list-disc list-inside ms-1">
+              <li v-for="ingredient in drinkIngredients" :key="ingredient.id">
+                {{ ingredient.text }}
+              </li>
+            </ul>
+            <h5 class="text-lg mt-5">作り方</h5>
+            <p class="mt-1">{{ drink.recipe_ja }}</p>
+            <div class="flex flex-row flex-wrap gap-2 mt-5">
+              <button
+                :class="[
+                  'btn btn-sm h-10 text-xs rounded-md',
+                  { favorite: isFavorite },
+                ]"
+                @click="updateFavorite()"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  :fill="isFavorite ? 'white' : 'none'"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                </svg>
+                お気に入り
+              </button>
+              <SocialShare
+                v-for="network in ['facebook', 'twitter', 'line']"
+                :key="network"
+                :network="network"
+                :styled="true"
+                :label="true"
+                class="text-white h-10"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <h5 class="text-2xl mt-20">おすすめ</h5>
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 mt-8">
-        <div v-for="drink in drinks" :key="drink.id">
-          <DrinkCard :drink="drink" />
+        <h5 class="text-2xl mt-20">おすすめ</h5>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 mt-8">
+          <div v-for="recommendedDrink in drinks" :key="recommendedDrink.id">
+            <DrinkCard :drink="recommendedDrink" />
+          </div>
         </div>
       </div>
     </div>
