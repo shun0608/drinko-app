d7f5493f88592cc40548ee7134422e2259357db9
shun0608 <bmss.4124@icloud.com>
2024-07-14 15:32:21 +0900

    DrinkCard.vue修正

--- nuxt/components/DrinkCard.vue
+++ nuxt/components/DrinkCard.vue
@@ -1,22 +1,25 @@
 <script setup>
 const props = defineProps({
-  drink: Object,
+  drink: {
+    type: Object,
+    default: () => ({}),
+  },
 });
 </script>
 
 <template>
-  <NuxtLink class="h-full block" :href="`/drinks/${drink.id}`">
+  <NuxtLink class="h-full block" :href="`/drinks/${props.drink.id}`">
     <div class="card card-compact bg-base100 shadow-xl h-full">
       <figure>
         <img
-          :src="drink.image_url"
-          :alt="drink.name_en"
+          :src="props.drink.image_url"
+          :alt="props.drink.name_en"
           class="aspect-square object-cover block w-full"
         />
       </figure>
       <div class="card-body">
-        <h2 class="card-title">{{ drink.name_en }}</h2>
-        <p>{{ drink.name_ja }}</p>
+        <h2 class="card-title">{{ props.drink.name_en }}</h2>
+        <p>{{ props.drink.name_ja }}</p>
       </div>
     </div>
   </NuxtLink>
