b332dc60118f62c8c6b801f768685a96d8d1d2d6
shun0608 <bmss.4124@icloud.com>
2024-07-14 14:20:33 +0900

    トップページindex.vue修正

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -16,61 +16,62 @@ const navigateToSearch = () => {
 </script>
 
 <template>
-  <UseSeoMeta />
-  <div class="bg-drink relative">
-    <div class="absolute bottom-5 start-4 md:inset-y-2/4 md:start-24">
-      <h1 class="text-5xl md:text-8xl font-black">Drinko</h1>
-      <h3 class="text-2xl md:text-4xl font-black mt-7">
-        丁寧に作ったドリンクで安らぎのひとときを
-      </h3>
-      <p class="mt-5">
-        Drinkoでは、世界各国のソフトドリンクとその作り方を調べることができます。<br />
-        <!-- 検索機能では、あなたの今の気分に合わせて、<br />
+  <div>
+    <UseSeoMeta />
+    <div class="bg-drink relative">
+      <div class="absolute bottom-5 start-4 md:inset-y-2/4 md:start-24">
+        <h1 class="text-5xl md:text-8xl font-black">Drinko</h1>
+        <h3 class="text-2xl md:text-4xl font-black mt-7">
+          丁寧に作ったドリンクで安らぎのひとときを
+        </h3>
+        <p class="mt-5">
+          Drinkoでは、世界各国のソフトドリンクとその作り方を調べることができます。<br />
+          <!-- 検索機能では、あなたの今の気分に合わせて、<br />
         厳選したソフトドリンクをオススメします。<br /> -->
-        アルコールが呑めない人にもドリンクの喜びを味わって欲しい。<br />
-        そんな願いを込めてつくりました。
-      </p>
-    </div>
-  </div>
-
-  <section class="my-28">
-    <SectionTitle>
-      <template #english>Recommended</template>
-      <template #japanese>おすすめ</template>
-    </SectionTitle>
-    <div class="mx-auto max-w-screen-lg mt-10">
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-7 mx-3">
-        <div v-for="drink in drinks" :key="drink.id">
-          <DrinkCard :drink="drink" />
-        </div>
+          アルコールが呑めない人にもドリンクの喜びを味わって欲しい。<br />
+          そんな願いを込めてつくりました。
+        </p>
       </div>
     </div>
-    <div class="flex justify-center mt-10">
-      <NuxtLink class="btn btn-wide btn-neutral" href="/drinks">
-        ドリンク一覧
-      </NuxtLink>
-    </div>
-  </section>
 
-  <section class="my-28 px-4">
-    <SectionTitle>
-      <template #english>Search</template>
-      <template #japanese>ドリンク検索</template>
-    </SectionTitle>
-    <div class="max-w-3xl mx-auto my-14 px-4 bg-gray-300 rounded-3xl">
-      <form @submit.prevent="navigateToSearch" class="py-14 mx-auto">
-        <label class="form-control w-full max-w-lg mx-auto">
-          <div class="label">
-            <span class="label-text">フリーワード</span>
+    <section class="my-28">
+      <SectionTitle>
+        <template #english>Recommended</template>
+        <template #japanese>おすすめ</template>
+      </SectionTitle>
+      <div class="mx-auto max-w-screen-lg mt-10">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-7 mx-3">
+          <div v-for="drink in drinks" :key="drink.id">
+            <DrinkCard :drink="drink" />
           </div>
-          <input
-            type="text"
-            v-model="keyword"
-            class="input input-bordered w-full max-w-lg"
-            required
-          />
-        </label>
-        <!-- <label class="form-control w-full max-w-lg mx-auto mt-3">
+        </div>
+      </div>
+      <div class="flex justify-center mt-10">
+        <NuxtLink class="btn btn-wide btn-neutral" href="/drinks">
+          ドリンク一覧
+        </NuxtLink>
+      </div>
+    </section>
+
+    <section class="my-28 px-4">
+      <SectionTitle>
+        <template #english>Search</template>
+        <template #japanese>ドリンク検索</template>
+      </SectionTitle>
+      <div class="max-w-3xl mx-auto my-14 px-4 bg-gray-300 rounded-3xl">
+        <form class="py-14 mx-auto" @submit.prevent="navigateToSearch">
+          <label class="form-control w-full max-w-lg mx-auto">
+            <div class="label">
+              <span class="label-text">フリーワード</span>
+            </div>
+            <input
+              v-model="keyword"
+              type="text"
+              class="input input-bordered w-full max-w-lg"
+              required
+            />
+          </label>
+          <!-- <label class="form-control w-full max-w-lg mx-auto mt-3">
           <div class="label">
             <span class="label-text">今の気分</span>
           </div>
@@ -83,12 +84,16 @@ const navigateToSearch = () => {
             <option>Star Trek</option>
           </select>
         </label> -->
-        <label class="form-control mt-14">
-          <button type="submit" class="btn btn-neutral w-full max-w-lg mx-auto">
-            検索
-          </button>
-        </label>
-      </form>
-    </div>
-  </section>
+          <label class="form-control mt-14">
+            <button
+              type="submit"
+              class="btn btn-neutral w-full max-w-lg mx-auto"
+            >
+              検索
+            </button>
+          </label>
+        </form>
+      </div>
+    </section>
+  </div>
 </template>
