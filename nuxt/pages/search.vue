64f7d41e3dfcd217f6cd19e819e657713bbb85d9
shun0608 <bmss.4124@icloud.com>
2024-07-14 13:53:03 +0900

    serch.vue及びterms-of-use.vue修正
    nuxt.d.tsの不要な記述削除

--- nuxt/pages/search.vue
+++ nuxt/pages/search.vue
@@ -65,57 +65,59 @@ onMounted(async () => {
 watch(route, fetchDrinks);
 
 watch(page, () => {
-  location.href = "/search?page=" + page.value + "&keyword=" + keyword;
+  location.href = "/search?page=" + page.value + "&keyword=" + keyword.value;
 });
 </script>
 
 <template>
-  <UseSeoMeta
-    :title="`${keyword}の検索結果`"
-    :description="`${keyword}の検索結果`"
-  />
-  <div class="max-w-screen-lg mx-auto mt-8">
-    <UBreadcrumb
-      :links="links"
-      :ui="{
-        wrapper: 'mx-4',
-        li: 'text-primary dark:text-primary',
-        active: 'text-primary dark:text-primary-400',
-        inactive: 'hover:text-gray-700 dark:hover:text-gray-200',
-      }"
+  <div>
+    <UseSeoMeta
+      :title="`${keyword}の検索結果`"
+      :description="`${keyword}の検索結果`"
     />
-  </div>
+    <div class="max-w-screen-lg mx-auto mt-8">
+      <UBreadcrumb
+        :links="links"
+        :ui="{
+          wrapper: 'mx-4',
+          li: 'text-primary dark:text-primary',
+          active: 'text-primary dark:text-primary-400',
+          inactive: 'hover:text-gray-700 dark:hover:text-gray-200',
+        }"
+      />
+    </div>
 
-  <div class="mx-auto max-w-screen-lg mt-10 min-h-dvh">
-    <div v-if="loading == false">
-      <div v-if="drinks.length > 0">
-        <div
-          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-3"
-        >
-          <div v-for="drink in drinks" :key="drink.id">
-            <DrinkCard :drink="drink" />
+    <div class="mx-auto max-w-screen-lg mt-10 min-h-dvh">
+      <div v-if="loading == false">
+        <div v-if="drinks.length > 0">
+          <div
+            class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-3"
+          >
+            <div v-for="drink in drinks" :key="drink.id">
+              <DrinkCard :drink="drink" />
+            </div>
+          </div>
+        </div>
+        <div v-else>
+          <div class="mx-4">
+            <p>検索結果が存在しません。</p>
           </div>
         </div>
       </div>
-      <div v-else>
-        <div class="mx-4">
-          <p>検索結果が存在しません。</p>
-        </div>
+      <div v-else class="flex justify-center">
+        <span class="loading loading-spinner loading-lg" />
       </div>
     </div>
-    <div v-else class="flex justify-center">
-      <span class="loading loading-spinner loading-lg"></span>
+    <div v-if="drinks.length > 0" class="flex justify-center">
+      <UPagination
+        v-model="page"
+        :page-count="12"
+        :total="parseInt(total)"
+        :to="generateLink"
+        :ui="{
+          wrapper: 'justify-center mt-24 mb-48 scale-125 sm:scale-150',
+        }"
+      />
     </div>
-  </div>
-  <div v-if="drinks.length > 0" class="flex justify-center">
-    <UPagination
-      v-model="page"
-      :page-count="12"
-      :total="parseInt(total)"
-      :to="generateLink"
-      :ui="{
-        wrapper: 'justify-center mt-24 mb-48 scale-125 sm:scale-150',
-      }"
-    />
   </div>
 </template>
